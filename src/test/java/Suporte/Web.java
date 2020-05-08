3841e24d576929bb7dac48a1960d5894e39e3812
Robson Lima da Rocha <robson.rocha@rsinet.com.br>
2020-05-07 22:34:47 -0300

    Configuração de Browserstak e Maven linha de comando

--- src/test/java/Suporte/Web.java
+++ src/test/java/Suporte/Web.java
@@ -2,10 +2,18 @@ package Suporte;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class Web {
+    public static final String USERNAME = "robsonrocha1";
+    public static final String AUTOMATE_KEY = "7CDZ7ndsygvBtVzqvSYV";
+    public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+
     public  static WebDriver createChrome(){
         //Abrindo o navegador
         System.setProperty("webdriver.chrome.driver","C:\\util\\drivers\\chromedriver.exe");
@@ -16,5 +24,31 @@ public class Web {
         navegador.get("http://www.juliodelima.com.br/taskit");
 
         return navegador;
+    }
+    public static  WebDriver createBrowserStack(){
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("browser", "Chrome");
+        caps.setCapability("browser_version", "60.0");
+        caps.setCapability("os", "Windows");
+        caps.setCapability("os_version", "10");
+        caps.setCapability("resolution", "1280x800");
+        caps.setCapability("name", "Bstack-[Java] Sample Test");
+        //caps.setCapability("browserstack.debug", "true");
+
+        WebDriver navegador = null;
+
+        try {
+            navegador = new RemoteWebDriver(new URL(URL), caps);
+            navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+            //Navegando para a página do taskit!
+            navegador.get("http://www.juliodelima.com.br/taskit");
+
+        }catch (MalformedURLException e){
+            System.out.println("Houveram problemas com a URL:" + e.getMessage());
+        }
+
+        return navegador;
+
     }
 }
